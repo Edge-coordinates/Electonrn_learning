84ab71550fd453c0684c29fa3a560dae352a1184
边缘坐标 <68896486+Edge-coordinates@users.noreply.github.com>
2023-01-26 11:22:38 +0800

    20230126

---
+++ Study_Lab/src/components/HomeNavigator.vue
@@ -0,0 +1,59 @@
+<template>
+    <el-scrollbar>
+        <el-button style="width:100%;" @click="OnClick(0)"><el-icon><MoreFilled/></el-icon></el-button>
+        <el-menu :default-openeds="['1']" default-activate="1-1">
+            <el-menu-item index="1-1">Option 1</el-menu-item>
+            <el-sub-menu index="2">
+                <template #title>
+                    <el-icon><icon-menu /></el-icon>Navigator Two
+                </template>
+                <el-menu-item-group>
+                    <template #title>Group 1</template>
+                    <el-menu-item index="2-1">Option 1</el-menu-item>
+                    <el-menu-item index="2-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                    <el-menu-item index="2-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="2-4">
+                    <template #title>Option 4</template>
+                    <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+            </el-sub-menu>
+            <el-sub-menu index="3">
+                <template #title>
+                    <el-icon><setting /></el-icon>Navigator Three
+                </template>
+                <el-menu-item-group>
+                    <template #title>Group 1</template>
+                    <el-menu-item index="3-1">Option 1</el-menu-item>
+                    <el-menu-item index="3-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                    <el-menu-item index="3-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="3-4">
+                    <template #title>Option 4</template>
+                    <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+            </el-sub-menu>
+        </el-menu>
+    </el-scrollbar>
+</template>
+
+<script setup lang="ts">
+    import { Menu as IconMenu, Message, Setting, MoreFilled} from '@element-plus/icons-vue'
+    let isCollapse:boolean = true
+</script>
+
+<script lang="ts">
+    export default {
+        methods: {
+            OnClick(index: number) {
+                if (index == 0){
+                    isCollapse = !isCollapse
+                }
+            }
+        }
+    }
+</script>
---
+++ Study_Lab/src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-container class="layout-container-demo">
+    <el-aside>
+      <HomeNavigator/>
+    </el-aside>
+    
+    <el-container>
+    <el-header style="text-align: right">
+        <PageHeader1/>    
+    </el-header>
+      <el-main>
+        <el-scrollbar>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="Date" width="140" />
+            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="address" label="Address" />
+          </el-table>
+        </el-scrollbar>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import PageHeader1 from '../components/PageHeader1.vue'
+import HomeNavigator from '@/components/HomeNavigator.vue';
+
+const item = {
+  date: '2016-05-02',
+  name: 'Tom',
+  address: 'No. 189, Grove St, Los Angeles',
+}
+const tableData = ref(Array.from({ length: 20 }).fill(item))
+</script>
+
+<style scoped>
+.layout-container-demo .el-header {
+  position: relative;
+  background-color: red;
+  /* background-color: var(--el-color-primary-light-7); */
+  /* color: var(--el-text-color-primary); */
+}
+.layout-container-demo .el-aside {
+  color: var(--el-text-color-primary);
+  /* background: var(--el-color-primary-light-8); */
+}
+.layout-container-demo .el-menu {
+  border-right: none;
+}
+.layout-container-demo .el-main {
+  padding: 0;
+}
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
+}
+</style>
